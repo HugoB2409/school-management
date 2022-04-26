d37b7fae54411719390477536e637f9fe4fd6fda
HugoB2409 <46574029+HugoB2409@users.noreply.github.com>
2022-04-26 14:14:50 -0400

    test connection db

---
+++ src/Database.java
@@ -0,0 +1,39 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+    private static final String DB_DRIVER = "oracle.jdbc.driver.OracleDriver";
+    private static final String DB_URL = "jdbc:oracle:thin:@localhost:1521/coursbd.uqtr.ca";
+    private static final String DB_USERNAME = "SMI1002_028";
+    private static final String DB_PASSWORD = "49gxje97";
+    private Connection connection;
+
+    public Database() {
+        connect();
+    }
+
+    private void connect() {
+        Connection conn = null;
+        try{
+            //Register the JDBC driver
+            Class.forName(DB_DRIVER);
+
+            //Open the connection
+            conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+
+            if(conn != null){
+                System.out.println("Successfully connected.");
+            }else{
+                System.out.println("Failed to connect.");
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        connection = conn;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws SQLException {
+        Database database = new Database();
+        Connection connection = database.getConnection();
+        String query = "SELECT * FROM Student";
+        PreparedStatement statement = connection.prepareStatement(query);
+        ResultSet rs = statement.executeQuery();
+        while (rs.next()) {
+            System.out.println(rs.getInt("id"));
+        }
     }
 }
